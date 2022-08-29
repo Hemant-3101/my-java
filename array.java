4fdd7efb29fa6d0252727cb14c2076b8453b291b
Hemant <68082167+Hemant-3101@users.noreply.github.com>
2022-08-30 00:38:55 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,16 @@
+package array;
+
+public class array {
+    public static void main(String[] args) {
+    	int marks[] = new int[3];
+    	marks[0]=25;
+    	marks[1]=50;
+    	marks[2]=100;
+    	
+    	System.out.println(marks[2]);
+    	System.out.println(marks[0]);
+    	
+    }
+	
+	
+}
