5c152c9080493902b10513a454cc761d3874fdb9
Hemant <68082167+Hemant-3101@users.noreply.github.com>
2022-08-30 13:01:53 +0530

    Create array intput.java

---
+++ array intput.java
@@ -0,0 +1,27 @@
+package array;
+import java.util.Scanner;
+
+
+public class array {
+public static void main(String[] args)
+{
+	Scanner sc=new Scanner(System.in);
+	int size=sc.nextInt();
+	int numbers[]=new int[size];
+	
+	for(int i=0; i<size; i++)
+	{
+	
+		numbers[i] = sc.nextInt();
+	
+	}
+	for(int i=0;i<size; i++)
+	{
+		System.out.println(numbers[i]);
+		
+	}
+	
+}
+	
+
+}
